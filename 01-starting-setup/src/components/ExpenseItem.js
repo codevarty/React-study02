42b5c8e7d0f2e449d9e673c57f9518d9ae1ef45b
codevarty <hisu7789@gmail.com>
2023-05-21 22:55:11 +0900

    session 01 update

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+// 비용 아이템 지정
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
+// 다른 파일에서 사용할 수 있도록 export
+export default ExpenseItem;
