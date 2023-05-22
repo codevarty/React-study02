8c1183e75d9586011b0ef2dac40375953f6c3eaa
codevarty <hisu7789@gmail.com>
2023-05-22 23:24:53 +0900

    session 01 update

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
 // 비용 아이템 지정
-function ExpenseItem() {
+import './ExpenseItem.css';
+// props 객체 매개 변수를 사용하여 데이터를 전달
+function ExpenseItem(props) {
+  // toLocaleString() 함수는 날짜 객체를 특정 언어에 맞는 문자열로 변환
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  // getFullYear() 함수는 날짜 객체의 연도를 4자리로 반환
+  const year = props.date.getFullYear();
   return (
-    <div>
-      <div>March 28th 2021</div>
+    <div className="expense-item">
       <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
