a4376f79df22200cfb38d5f648be0fc5405841db
codevarty <hisu7789@gmail.com>
2023-05-30 00:25:36 +0900

    ExpressItem.js 파일 수정

--- session-02/01-starting-setup/src/components/Expense/ExpenseItem.js
+++ session-02/01-starting-setup/src/components/Expense/ExpenseItem.js
@@ -1,11 +1,11 @@
 // 비용 아이템 지정
-import React, {useState} from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 // props 객체 매개 변수를 사용하여 데이터를 전달
 const ExpenseItem = (props) => {
-  // state를 생성하고 초기값을 설정
+  // 무상태 컴포넌트는 읽기 전용이므로 상태를 변경할 수 없다.
   return (
     <Card className="expense-item">
       <ExpenseDate
