62c54625521882dfb2e462933f311e794a4b80e9
codevarty <hisu7789@gmail.com>
2023-06-06 00:31:46 +0900

    session 05 update

---
+++ session-05/01-starting-project/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      // id: goal1 이라는 논리적인 오류 발생
+      // 아이디는 하나의 고유한 값이어야 한다.
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+  /*
+  반드시 jsx 코드는 루트 요소가 있어야 한다.
+  두 개 이상의 jsx 코드는 반환 할 수 없다.
+  return (
+    <section>
+    </section>
+    <section>
+    </section>
+  )
+   */
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
+    </div>
+  );
+};
+
+export default App;
