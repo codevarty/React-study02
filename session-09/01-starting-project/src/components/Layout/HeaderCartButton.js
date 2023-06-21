958a79025100f9fab4f622254e2dfbd74b8857c7
codevarty <hisu7789@gmail.com>
2023-06-21 20:52:02 +0900

    session 09 update

--- session-09/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ session-09/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
+  const {items} = cartCtx;
   //cartCtx.items.reduce()를 사용하여 cartCtx.items의 각 요소의 amount를 더한다.
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0)
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(()=> {
+    if(items.length === 0) { 
+      return;
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300);
+
+    // 클린업 함수를 사용하는 것이 좋다.
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+  
   return (
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
--- session-09/01-starting-project/src/store/CartProvider.js
+++ session-09/01-starting-project/src/store/CartProvider.js
@@ -9,26 +9,75 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
+  if (action.type === "ADD") {
     // 기존 state.totalAmount에 action.item.price * action.item.amount를 더한다.
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    // 기존 state.items에 action.item.id와 같은 id가 존재하는지 확인한다.
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    // 기존에 있는 item이 없으면 undefined를 반환한다.
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      // 아이템에 수량을 업데이트한다.
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if(existingItem.amount ===1) {
+      // 개수를 줄이는 아이템만 제거되도록 filter를 사용한다.
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
     }
   }
   return defaultCartState;
-}
+};
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction]  =useReducer(cartReducer, defaultCartState);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({type: 'ADD', item: item});
+    dispatchCartAction({ type: "ADD", item: item });
   };
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: 'REMOVE', id: id});
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
   const cartContext = {
     items: cartState.items,
