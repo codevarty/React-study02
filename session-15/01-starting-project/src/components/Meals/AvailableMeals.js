b93be24bbe8cdb547b1dcb3203b2a30e2982bbb3
codevarty <hisu7789@gmail.com>
2023-07-16 21:18:23 +0900

    session 15 create file

---
+++ session-15/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const [ meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading]  = useState(true);
+  // useEffect 함수 내에서는 async await를 사용할 수 없다.
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-5ac8d-default-rtdb.firebaseio.com/meals.json');
+      const data = await response.json();
+      // console.log(data);
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+    setIsLoading(false);
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+  
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
