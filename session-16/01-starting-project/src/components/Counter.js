4939b2c01679fd607f4c2c082a18d0c8689ee223
codevarty <hisu7789@gmail.com>
2023-07-24 16:17:20 +0900

    react-redux 사용 방법 정리

--- session-16/01-starting-project/src/components/Counter.js
+++ session-16/01-starting-project/src/components/Counter.js
@@ -1,15 +1,70 @@
-import classes from './Counter.module.css';
 
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+
+// 함수형 컴포넌트
 const Counter = () => {
+  const dispatch = useDispatch(); // useDispatch() 함수를 사용하여 dispatch 함수를 가져온다. (no need to argument)
+  const counter = useSelector((state) => state.counter); // useSelector() 함수를 사용하여 state.counter 값을 가져온다.
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
-
 export default Counter;
+
+// 클래스 컴포넌트 (자주 사용되지 않는다.)
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- session-16/redux.md
+++ session-16/redux.md
@@ -1,11 +1,14 @@
 # 리덕스 라이브러리
 
 ## 리덕스 라이브러리 사용 및 방법
+
 ### 리덕스 정의
+
 - 리덕스는 자바스크립트 앱을 위한 예측 가능한 상태 컨테이너
 - 리덕스는 상태를 관리하는 라이브러리
 
 ### 리액트 3가지 상태
+
 - local state
   - 컴포넌트 내에 사용가능한 상태
   - 보통 useState, useReducer를 사용하여 관리
@@ -13,17 +16,20 @@
   - 다수의 컴포넌트에 영향을 미치는 상태
   - props chain, prop drilling을 사용하여 관리
 - App-wide state
+
   - 앱 전체에 영향을 미치는 상태
   - props chain, prop drilling을 사용하여 관리
 
 - 위의 상태들 중 cross component state와 App-wide state를 관리하기 위해 리덕스를 사용
 
 ### 리액트 컨텍스트 대신 리덕스 사용 이유
+
 - 리액트 컨텍스트 잠재적 단점
   - 컨텍스트를 사용할 때 상태 관리가 복잡해질 수 있음
   - 데이터가 자주 변경되는 경우 리액트 컨텍스트는 성능이 떨어질 수 있음
 
 ### 리덕스 작동 방식
+
 - 리덕스는 한 개의 스토어를 가짐(리액트 앱의 모든 상태 데이터를 가짐)
 - 컴포넌트에서 상태를 가져오는 것을 구독(subscribe)이라고 함
 - 상태를 변경하는 방법 (직접 변경 불가능)
@@ -32,6 +38,7 @@
   - 디스패치된 액션은 리듀서 함수에 전달된다.
 
 ### 리덕스 설치 및 사용
+
 - `npm install redux`명령어로 리덕스 설치
 - 리액트와 함께 사용하기 위해 `npm install redux react-redux`명령어로 react-redux도 함께 설치
 - `const redux = require('redux')`로 리덕스 불러오기 (node.js 구문)
@@ -46,5 +53,16 @@
   - `store.subscribe()`함수를 사용하여 스토어의 상태가 변경될 때마다 호출되는 함수를 등록할 수 있음
 - dispatch
   - `store.dispatch()`함수를 사용하여 액션을 디스패치할 수 있음
+- 리액트 앱에서 리덕스 사용
+  - `import { Provider } from 'react-redux'`로 리덕스 불러오기 (상단의 컴포넌트를 감싸준다.)
+  - `useSelector` 함수를 사용하여 리덕스 스토어의 상태를 가져올 수 있음 (argument: function , return: state)
+  - `useDispatch` 함수를 사용하여 리덕스 스토어의 액션을 디스패치할 수 있음 (argument: Not need, return: function)
+- 클래스 컴포넌트에서 리덕스 사용
+  - `connect` 함수를 통해 리덕스 스토어의 상태와 액션을 props로 매핑할 수 있음
+    - `export defualt connect(mapStateToProps, mapDispatchToProps)(Component))` 형태로 사용
+    - `mapStateToProps` 함수를 사용하여 리덕스 스토어의 상태를 props로 매핑할 수 있음
+    - `mapDispatchToProps` 함수를 사용하여 리덕스 스토어의 액션을 props로 매핑할 수 있음
+  - `this.props.dispatch()`함수를 사용하여 액션을 디스패치할 수 있음
+  - `this.props.state`함수를 사용하여 리덕스 스토어의 상태를 가져올 수 있음
 
 ## 리덕스 사용 Toolkit
